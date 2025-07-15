698d3c028c226ec0b4be2f46d2b33b2827a5e136
KGH93 <howls4427@gmailc.com>
2025-07-15 18:02:32 +0900

    0715C

--- spring/Final/wooriZip/src/main/java/com/example/demo/repository/RecommendLogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.RecommendLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecommendLogRepository extends JpaRepository<RecommendLog, Long> {
-
-}
