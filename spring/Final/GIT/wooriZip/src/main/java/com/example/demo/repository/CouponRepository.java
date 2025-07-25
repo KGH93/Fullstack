ea952a684300e063c15836b82e71ff35e7e73ecf
KGH93 <howls4427@gmailc.com>
2025-07-25 18:21:26 +0900

    0725

--- spring/Final/GIT/wooriZip/src/main/java/com/example/demo/repository/CouponRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Coupon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CouponRepository extends JpaRepository<Coupon, Long> {
-
-}
--- spring/Final/GIT/wooriZip/src/main/java/com/example/demo/repository/ProductAttributeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.ProductAttribute;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductAttributeRepository extends JpaRepository<ProductAttribute, Long> {
-}
